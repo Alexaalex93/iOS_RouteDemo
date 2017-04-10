150d09af50cb502c6412f568b965dbb405569814
cice <formador@aula1PovMacNFOR.local>
2017-04-10 08:51:32 +0200

    Initial Commit

---
+++ RouteDemo/RouteViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RouteViewController.swift
+//  RouteDemo
+//
+//  Created by Pablo Mateo Fernández on 02/02/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RouteViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
